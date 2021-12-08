8a0959460579f80368306fa364cbe106a871c16e
SilasAggrey <91203655+SilasAggrey@users.noreply.github.com>
2021-12-08 03:30:08 -1200

    react-class-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.email);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Email</label>
+        <input
+          type="email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
+
+
+
+
 
 export default App;
